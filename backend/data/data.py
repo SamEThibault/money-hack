00bd21651e70a25a38df2ea735fe96e019ee1736
LoganHoogendijk <logan.hoogendijk@gmail.com>
2022-08-26 20:53:14 -0400

    convert ipynb to py

---
+++ backend/data/data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import re
+import numpy as np
+
+class DataFormat():
+
+    def format_txt_to_csv(self):
+        with open('myBadSpendingHabits.txt', 'r') as file:
+            filedata = file.read()
+
+            filedata = filedata.replace(',' , '.')
+
+        with open('myBadSpendingHabitsFormat.txt', 'w') as file:
+            file.write(filedata)
+
+        with open('myBadSpendingHabitsFormat.txt') as infile:
+            data = re.sub('\t+', ',', infile.read())
+            data = data.replace(',\n', '\n')
+
+            print(data, file=open('spendingHabits.csv', 'w'))   
+
+        read_file = pd.read_csv(r'spendingHabits.csv')
+        read_file.to_csv(r'spendingHabits.csv', index=False, header=["Transaction Date", "Registration Date", "Transaction Number", "Description", "Amount ($)"]) 
+
+    def parseToDict(self):
+        info = pd.read_csv(r"spendingHabits.csv")
+        dates = info['Transaction Date'].values
+        desc = info['Description'].values
+        amount = info['Amount ($)'].values      
+        datesList = dates.tolist()
+        descList = desc.tolist()
+        amountList = amount.tolist()
+        userInfo = {"dates": datesList, "description": descList, "amount": amountList}
+        
